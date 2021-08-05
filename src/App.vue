90580808aa37a0b73bc470cb99df3426459fc702
Aldon <aisenberg@freightview.com>
2021-08-05 01:34:42 -0500

    funny stuff

--- src/App.vue
+++ src/App.vue
@@ -73,13 +73,13 @@
     </Disclosure>
 
     <div class="py-10">
-      <header>
+      <!-- <header>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 class="text-3xl font-bold leading-tight text-gray-900">
             Dashboard
           </h1>
         </div>
-      </header>
+      </header> -->
       <main>
         <router-view />
       </main>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,73 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div>HOME PAGE</div>
-  <ul>
-    <li><router-link to="/wrestlers">Wrestlers</router-link></li>
-    <li><router-link to="/league-events">League Events</router-link></li>
-  </ul>
+  <div class="max-w-lg mx-auto">
+    <h2 class="text-lg font-medium text-gray-900">Create your first project</h2>
+    <p class="mt-1 text-sm text-gray-500">Get started by selecting a template or start from an empty project.</p>
+    <ul role="list" class="mt-6 border-t border-b border-gray-200 divide-y divide-gray-200">
+      <li v-for="(item, itemIdx) in items" :key="itemIdx">
+        <div class="relative group py-4 flex items-start space-x-3">
+          <div class="flex-shrink-0">
+            <span :class="[item.iconColor, 'inline-flex items-center justify-center h-10 w-10 rounded-lg']">
+              <component :is="item.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+            </span>
+          </div>
+          <div class="min-w-0 flex-1">
+            <div class="text-sm font-medium text-gray-900">
+              <a :href="item.href">
+                <span class="absolute inset-0" aria-hidden="true" />
+                {{ item.name }}
+              </a>
+            </div>
+            <p class="text-sm text-gray-500">{{ item.description }}</p>
+          </div>
+          <div class="flex-shrink-0 self-center">
+            <ChevronRightIcon class="h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="mt-6 flex">
+      <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">Or start from an empty project<span aria-hidden="true"> &rarr;</span></a>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { ChevronRightIcon } from '@heroicons/vue/solid'
+import { CalendarIcon, SpeakerphoneIcon, TerminalIcon } from '@heroicons/vue/outline'
 
+const items = [
+  {
+    name: 'Add a Wrestler',
+    description: 'Looks like we got a new wrestler.',
+    href: 'wrestlers/add',
+    iconColor: 'bg-pink-500',
+    icon: SpeakerphoneIcon,
+  },
+  {
+    name: 'Add League Event',
+    description: 'We got some tough guys who need to prove themselves.',
+    href: 'league-events/create',
+    iconColor: 'bg-purple-500',
+    icon: TerminalIcon,
+  },
+  {
+    name: 'View Events',
+    description: 'Here you will find a list of events.',
+    href: '/league-events',
+    iconColor: 'bg-yellow-500',
+    icon: CalendarIcon,
+  },
+]
+
+export default {
+  components: {
+    ChevronRightIcon,
+  },
+  setup() {
+    return {
+      items,
+    }
+  },
 }
 </script>
-
-<style>
-
-</style>
